a3548ed5332cccc620f9f84b1bc8a07c61f1fe11
Gregory Iosifidis <travletothefurureprogramming@gmail.com>
2026-06-15 16:44:54 +0300

    Add network usage metrics to system info and update dashboard display

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 import psutil
+import time
 
 def get_cpu():
     return psutil.cpu_percent(interval=0.5)
@@ -12,6 +13,20 @@ def get_disk_usage():
     usage = psutil.disk_usage('/')
     return usage.percent
 
+def get_network_usage():
+    sent1 = psutil.net_io_counters().bytes_sent
+    recv1 = psutil.net_io_counters().bytes_recv
+
+    time.sleep(1)
+
+    sent2 = psutil.net_io_counters().bytes_sent
+    recv2 = psutil.net_io_counters().bytes_recv
+
+    bytes_sent = sent2 - sent1
+    bytes_recv = recv2 - recv1
+
+    return bytes_sent,bytes_recv
+    
 
 server = Flask(__name__)
 CORS()
@@ -25,13 +40,18 @@ def return_system_info():
     cpu = get_cpu()
     ram = get_ram()
     disk = get_disk_usage()
+    net = get_network_usage()
 
     system_info = {
         "cpu":cpu,
         "ram":ram,
-        "disk":disk
+        "disk":disk,
+        "net_sent": net[0],
+        "net_recv": net[1]
     }
 
     return jsonify(system_info)
+
+
 
 server.run("0.0.0.0","8080")
